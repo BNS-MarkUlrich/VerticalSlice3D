1c0a184ed219eddc119fdd38a15cb99c0a2333a9
Mark Ulrich <31822@ma-web.nl>
2021-12-22 23:20:51 +0100

    Pretty much done, just need to polish it and clean code

--- Assets/Scripts/PerspectiveTransform/PerspectiveTransform.cs
+++ Assets/Scripts/PerspectiveTransform/PerspectiveTransform.cs
@@ -19,11 +19,16 @@ public class PerspectiveTransform : MonoBehaviour
 
     [Header("Mouse Position")]
     [SerializeField] private Vector3 mouseWorldPoint;
+    [SerializeField] private RaycastHit collisionHit;
 
     [Header("Scale & Distance")]
     [SerializeField] private float newDistance;
     [SerializeField] private float originalDistance;
     [SerializeField] private float scaleModifier;
+    [SerializeField] private float newScaleModifier;
+
+    [SerializeField] private float oldSelectionDistance;
+    [SerializeField] private float selectionDistance;
 
     [SerializeField] private RaycastHit boxHit;
 
@@ -48,25 +53,26 @@ public class PerspectiveTransform : MonoBehaviour
         LayerMask mask = LayerMask.GetMask("Selectables");
 
         /// Call Selection/Pickup System here
-        if (Physics.Raycast(ray, out hitData, 1000) && hitData.transform.tag == "Selectable")
+        if (Physics.Raycast(ray, out collisionHit, 1000))
         {
-            Vector3 mouseLocalPoint = Vector3.MoveTowards(transform.position, transform.position + transform.forward * hitData.distance, hitData.distance);
-            mouseWorldPoint = new Vector3(hitData.point.x, hitData.point.y, hitData.point.z);
-            Debug.DrawLine(this.gameObject.transform.position, hitData.point, Color.red);
+            Vector3 mouseLocalPoint = Vector3.MoveTowards(transform.position, transform.position + transform.forward * collisionHit.distance, collisionHit.distance);
+            mouseWorldPoint = new Vector3(collisionHit.point.x, collisionHit.point.y, collisionHit.point.z);
+            Debug.DrawLine(this.gameObject.transform.position, collisionHit.point, Color.blue);
             
         }
-        newDistance = hitData.distance;
+        newDistance = collisionHit.distance;
 
         switch (currentState)
         {
             case States.SelectionState:
-                //scaleModifier = 0;
+                scaleModifier = 0;
                 if (Input.GetMouseButtonDown(0)) // Must select object to begin. Pickup system
                 {
-                    if (Physics.Raycast(ray, out hitData, 1000, mask) && hitData.transform.tag == "Selectable")
+                    if (Physics.Raycast(ray, out hitData, 1000) && hitData.transform.tag == "Selectable")
                     {
                         selectedObject = hitData.transform.gameObject; // Pickup system
                         selectedRigidBody = selectedObject.GetComponent<Rigidbody>();
+                        selectedObject.layer = 2;
                         currentState = States.InitialiseState;
                     }
                 }
@@ -87,11 +93,13 @@ public class PerspectiveTransform : MonoBehaviour
                 break;
             case States.GrabbedState:
                 /// Grabbed Begin
+                oldSelectionDistance = Vector3.Distance(selectedObject.GetComponent<Collider>().bounds.center, Camera.main.transform.position);
+                selectionDistance = Vector3.Distance(collisionHit.point, Camera.main.transform.position);
+                newScaleModifier = oldSelectionDistance / selectionDistance;
                 scaleModifier = newDistance / originalDistance;
-                selectedParent.transform.localScale = originalScale * scaleModifier;
+                selectedObject.transform.localScale = (originalScale * scaleModifier) * newScaleModifier;
                 selectedObject.transform.rotation = new Quaternion(0,0,0,1); // Pickup system remove/change
-                LayerMask boxMask = LayerMask.GetMask("Default");
-                collisionDetected = Physics.BoxCast(selectedParent.GetComponent<Collider>().bounds.center, transform.localScale, transform.forward, out boxHit, transform.rotation = Quaternion.identity, 1000, boxMask);
+                collisionDetected = Physics.BoxCast(selectedParent.GetComponent<Collider>().bounds.center, transform.localScale/2, transform.forward, out boxHit, transform.rotation = Quaternion.identity, newDistance);
                 if (collisionDetected)
                 {
                     Vector3 mouseLocalPoint = Vector3.MoveTowards(transform.position, transform.position + transform.forward * boxHit.distance, boxHit.distance);
@@ -102,8 +110,9 @@ public class PerspectiveTransform : MonoBehaviour
                 {
                     collisionDetected = false;
                     selectedRigidBody.GetComponent<Collider>().isTrigger = false;
+                    selectedObject.layer = 0;
                     selectedObject.transform.parent = transform.parent;  // Pickup system
-                    selectedObject.transform.position = mouseWorldPoint;
+                    selectedObject.transform.position = boxHit.point;
                     selectedObject.transform.rotation = new Quaternion(0, 0, 0, 1); // Pickup system remove/change
                     selectedRigidBody.isKinematic = false;
                     selectedRigidBody.useGravity = true;
@@ -139,9 +148,9 @@ public class PerspectiveTransform : MonoBehaviour
         if (collisionDetected)
         {
             //Draw a Ray forward from GameObject toward the hit
-            Gizmos.DrawRay(transform.position, transform.forward * boxHit.distance);
+            //Gizmos.DrawRay(transform.position, transform.forward * boxHit.distance);
             //Draw a cube that extends to where the hit exists
-            Gizmos.DrawWireCube(transform.position + transform.forward * boxHit.distance, transform.localScale);
+            //Gizmos.DrawWireCube(transform.position + transform.forward * boxHit.distance, transform.localScale);
         }
     }
 }
