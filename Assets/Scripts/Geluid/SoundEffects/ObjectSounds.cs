b97d51e5b29b1cf8e7bcbec6f0223b7477e2f79e
Minoord <minoord18@outlook.com>
2022-01-13 15:17:16 +0100

    when a box hit the floor it makes sound and i started with the pion sound too

--- Assets/Scripts/Geluid/SoundEffects/ObjectSounds.cs
+++ Assets/Scripts/Geluid/SoundEffects/ObjectSounds.cs
@@ -1,55 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class ObjectSounds : MonoBehaviour
 {
-    public bool MakeSound;
+    public AudioClip smallAudio;
+    public AudioClip mediumAudio;
+    public AudioClip bigAudio;
 
     private AudioClip _fallingSound;
     private AudioSource _audioSource;
     private float scaleOfObject;
 
+    private void Start()
+    {
+        _audioSource = FindObjectOfType<AudioSource>();
+    }
     void Update()
     {
         scaleOfObject = transform.localScale.x * transform.localScale.y * transform.localScale.z;
-        if(scaleOfObject <= 1000)
+        if (scaleOfObject <= 1000)
         {
-            _fallingSound = Resources.Load<AudioClip>("");
+            _fallingSound = smallAudio;
             Debug.Log("Small");
         }
         else if(scaleOfObject <= 8000)
         {
-            _fallingSound = Resources.Load<AudioClip>("");
+            _fallingSound = mediumAudio;
             Debug.Log("medium");
         }
-        else if (scaleOfObject <= 27000)
+        else 
         {
-            _fallingSound = Resources.Load<AudioClip>("");
+            _fallingSound = bigAudio;
             Debug.Log("Big");
-        }
-        else
-        {
-            _fallingSound = Resources.Load<AudioClip>("");
-            Debug.Log("Huge");
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(MakeSound == true)
+        if(collision.relativeVelocity.magnitude > 1)
         {
-            //_audioSource.PlayOneShot(_fallingSound);
-            Debug.Log("play music");
-            MakeSound = false;
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if( MakeSound == false)
-        {
-            MakeSound = true;
+            _audioSource.PlayOneShot(_fallingSound);
         }
     }
 }
