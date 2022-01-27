db9d33a8447f95fe16924b6d088c87b1e05a119e
Minoord <minoord18@outlook.com>
2022-01-27 13:50:45 +0100

    Pion can scale and move when picked up

--- Assets/Scripts/PerspectiveTransform/NewPersTransform.cs
+++ Assets/Scripts/PerspectiveTransform/NewPersTransform.cs
@@ -32,6 +32,7 @@ public class NewPersTransform : MonoBehaviour
     protected RaycastHit boxHit;
     protected RaycastHit[] boxHits;
 
+
     protected Collider[] overlapHits;
 
     [SerializeField] protected Vector3 originalBoxCastPos;
@@ -74,7 +75,7 @@ public class NewPersTransform : MonoBehaviour
                 scaleModifier = 1; // Reset scale modifier
                 if (Input.GetMouseButtonDown(0)) // Must select object to begin.
                 {
-                    if (Physics.Raycast(ray, out hitData, 1000) && hitData.transform.tag == "Selectable") // Check for selectables
+                    if (Physics.Raycast(ray, out hitData, 1000) && hitData.transform.tag == "SelectableBox" || hitData.transform.tag == "SelectableBlock" || hitData.transform.tag == "SelectablePion" || hitData.transform.tag == "SelectableCB") // Check for selectables
                     {
                         selectedObject = hitData.transform.gameObject; // Selected object is now grabbed
                         selectedRigidBody = selectedObject.GetComponent<Rigidbody>();
@@ -124,10 +125,21 @@ public class NewPersTransform : MonoBehaviour
                     if (i < 10)
                     {
                         newBoxCastPos = Vector3.MoveTowards(originalBoxCastPos, originalBoxCastPos + transform.forward * incrementDistance, incrementDistance); // Calculate optimal position
-                        boxHits = Physics.BoxCastAll(originalBoxCastPos, (originalScale * scaleModifier)/2, transform.forward, selectedObject.transform.rotation, incrementDistance);
+                        if (selectedObject.tag == "SelectableBox" || selectedObject.tag == "SelectableBlock")
+                        {
+                            boxHits = Physics.BoxCastAll(originalBoxCastPos, (originalScale * scaleModifier) / 2, transform.forward, selectedObject.transform.rotation, incrementDistance);
+                        }
+                        else if ( selectedObject.tag == "SelectablePion" || selectedObject.tag == "SelectableCB")
+                        {
+                            Vector3 p1 = originalBoxCastPos + selectedObject.GetComponent<CapsuleCollider>().center + Vector3.up * -selectedObject.transform.localScale.y * 0.5f;
+                            Vector3 p2 = p1 + Vector3.up * selectedObject.GetComponent<CapsuleCollider>().height/4;
+                            float capsuleCollider = selectedObject.GetComponent<CapsuleCollider>().radius /2;
+                            boxHits = Physics.CapsuleCastAll( p1,p2, capsuleCollider, transform.forward, incrementDistance);
+                        }
                         shortestDistance = Vector3.Distance(originalBoxCastPos, newBoxCastPos); ; // Max short distance
                         for (int y = 0; y < boxHits.Length; y++) // Get minimal distance in array
                         {
+                            Debug.DrawLine(transform.position, boxHits[y].point);
                             //Debug.Log(boxHits[y].transform.name);
                             if (shortestDistance > boxHits[y].distance && boxHits[y].transform.tag != "Player")
                             {
@@ -191,7 +203,7 @@ public class NewPersTransform : MonoBehaviour
             //Draw a Ray forward from GameObject toward the hit
             //Gizmos.DrawRay(transform.position, transform.forward * boxHit.distance);
             //Draw a cube that extends to where the hit exists
-            Gizmos.DrawWireCube(selectedObject.transform.position, originalScale * scaleModifier);
+            //Gizmos.DrawWireCube(selectedObject.transform.position, originalScale * scaleModifier);
             //Gizmos.DrawMesh(selectedObject.GetComponent<Mesh>(), transform.position + transform.forward * boxHit.distance, selectedParent.transform.rotation, selectedParent.transform.localScale);
         }
     }
